866db3ed889350c13b7ec34326b2563620218374
Tom Hayden <tomhaydenss@gmail.com>
2022-09-29 16:49:17 -0300

    Improve readability

--- challenge_1/lib/solution.ex
+++ challenge_1/lib/solution.ex
@@ -3,21 +3,14 @@ defmodule Solution do
       when is_list(first_grades) and is_list(second_grades) do
     summary =
       Enum.zip(first_grades, second_grades)
-      |> Enum.reduce(%{first: 0, second: 0}, fn grades_to_compare, result ->
-        case grades_to_compare do
-          {first_grade, second_grade} when first_grade > second_grade ->
-            %{result | first: result.first + 1}
-
-          {first_grade, second_grade} when second_grade > first_grade ->
-            %{result | second: result.second + 1}
-
-          _ ->
-            result
-        end
-      end)
+      |> Enum.reduce(%{first: 0, second: 0}, &compare_and_count/2)
 
     [summary.first, summary.second]
   end
 
   def compare_grades(_, _), do: {:error, :invalid_parameters}
+
+  defp compare_and_count({first_grade, second_grade}, result) when first_grade > second_grade, do: %{result | first: result.first + 1}
+  defp compare_and_count({first_grade, second_grade}, result) when second_grade > first_grade, do: %{result | second: result.second + 1}
+  defp compare_and_count(_, result), do: result
 end
