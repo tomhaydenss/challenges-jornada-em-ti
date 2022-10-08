b56e56e47c3098646166e89ad0744b671a2408ab
Tom Hayden <tomhaydenss@gmail.com>
2022-10-08 00:58:44 -0300

    Create project challenge_2

---
+++ challenge_2/lib/challenge2.ex
@@ -0,0 +1,18 @@
+defmodule Challenge2 do
+  @moduledoc """
+  Documentation for `Challenge2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Challenge2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ challenge_2/test/challenge2_test.exs
@@ -0,0 +1,8 @@
+defmodule Challenge2Test do
+  use ExUnit.Case
+  doctest Challenge2
+
+  test "greets the world" do
+    assert Challenge2.hello() == :world
+  end
+end
